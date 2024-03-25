0fe6f7807643810fe6bb00a5d236e64dd7eff823
Giovana Merguizo <giovana.merguizo@facens.br>
2024-03-25 17:36:13 -0300

    First Commit

---
+++ src/app/featureds/featureds.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featureds',
+  templateUrl: './featureds.component.html',
+  styleUrl: './featureds.component.css'
+})
+export class FeaturedsComponent {
+
+}
